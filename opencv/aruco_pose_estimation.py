2520292e54e48e0c7d960ead8fec097ede884b91
tmddn833 <tmddn833@korea.ac.kr>
2021-01-27 10:52:20 +0900

    aruco marker complete

--- opencv/aruco_pose_estimation.py
+++ opencv/aruco_pose_estimation.py
@@ -77,13 +77,40 @@ def rotationMatrixToEulerAngles(R):
 
     return np.array([x, y, z])
 
+def gstreamer_pipeline(
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
+    framerate=60,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
 
 
 
 #--- Get the camera calibration path
-calib_path  = ""
-camera_matrix   = np.loadtxt(calib_path+'cameraMatrix_webcam.txt', delimiter=',')
-camera_distortion   = np.loadtxt(calib_path+'cameraDistortion_webcam.txt', delimiter=',')
+
+camera_matrix   = np.loadtxt('/home/jetson/drone/opencv/camera_01/cameraMatrix.txt', delimiter=',')
+camera_distortion   = np.loadtxt('/home/jetson/drone/opencv/camera_01/cameraDistortion.txt', delimiter=',')
 
 #--- 180 deg rotation matrix around the x axis
 R_flip  = np.zeros((3,3), dtype=np.float32)
@@ -97,7 +124,7 @@ parameters  = aruco.DetectorParameters_create()
 
 
 #--- Capture the videocamera (this may also be a video or a picture)
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
 #-- Set the camera size as the one it was calibrated with
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
--- opencv/save_snapshots.py
+++ opencv/save_snapshots.py
@@ -23,15 +23,50 @@ import os
 __author__ = "Tiziano Fiorenzani"
 __date__ = "01/06/2018"
 
+def gstreamer_pipeline(
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
+    framerate=60,
+    flip_method=0,
+    
+    # capture_width=3264,
+    # capture_height=2464,
+    # display_width=3264,
+    # display_height=2464,
+    # framerate=21,
+    # flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
 
 def save_snaps(width=0, height=0, name="snapshot", folder=".", raspi=False):
 
     if raspi:
         os.system('sudo modprobe bcm2835-v4l2')
 
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
     if width > 0 and height > 0:
         print("Setting the custom Width and Height")
+        print(cap.isOpened())
+
         cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
         cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
     try:
